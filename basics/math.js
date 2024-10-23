51f59e0f5aadfc4a2dcc6124fd63830648e43dd3
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2024-10-23 15:34:23 +0000

    math bhi hogai

---
+++ basics/math.js
@@ -0,0 +1,26 @@
+console.log(Math); 
+// inspect element mein same cheeze likh k kafi sare functions access kiye ja skte hai 
+
+
+console.log(Math.abs(-69));
+console.log(Math.round(6.9));
+console.log(Math.ceil(6.1))
+// thoda sa bhi jyada hua toh bhi greater value mein hi roundoff hoga 
+console.log(Math.floor(6.9));
+// hamesha niche wali value lega 
+
+
+console.log(Math.random());
+// hamesha value 0 se 1 ke beech mein aegi 
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+// important hai yaad rkhio 
