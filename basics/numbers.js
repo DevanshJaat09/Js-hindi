a781250e9083ec141e30995055b974108704105b
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2024-10-23 15:15:38 +0000

    number completed

---
+++ basics/numbers.js
@@ -0,0 +1,28 @@
+const score = 69
+
+const balance = new Number(100);
+
+// variables define krne k do tareeke 
+console.log(score);
+console.log(balance);
+// bas thoda sa difference aega 
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+const otherNumber = 123.8966 
+
+console.log(otherNumber.toPrecision(4));
+
+const hundereds = 1000000
+console.log(hundereds.toLocaleString());
+// indian values use krne k liye 
+console.log(hundereds.toLocaleString('en-IN'));
+
+// max or min values console pe jake dekhiye 
+// sare methods console mein Numbers. likh k access kiye ja skte hai 
+
+
+
+
