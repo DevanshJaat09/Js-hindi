e817bc69f389a2bd58e881bc1bf6732759ee2912
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2024-10-29 11:32:43 +0000

    object's basics done

---
+++ basics_2/1_objects.js
@@ -0,0 +1,44 @@
+//  agar literals ki tarah objects declare kroge toh singleton nhi banega,
+//  constructor se banaoge toh singleton banega
+
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    // symbol declare karne ka tareeka dhyan rkhio 
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// ye hai objects mein value badalne ka tareeka 
+Object.freeze(JsUser)
+// freeze krne ke baad changes propogate nhi honge 
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
