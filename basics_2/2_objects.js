dbebc985af4b00bb42f19838f69272aa5d332fd9
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2024-10-30 06:26:16 +0000

    json api

--- basics_2/2_objects.js
+++ basics_2/2_objects.js
@@ -54,6 +54,7 @@ users[1].email
 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
+// -------------------------object destructuring and json api--------------------------------------------
 
 const course = {
     coursename: "js in hindi",
