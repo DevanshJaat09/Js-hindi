dd0dea64ea22b1547441ff83d33582e01a454db0
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2024-11-17 15:11:12 +0000

    done

---
+++ 4_control_flow/one.js
@@ -0,0 +1,17 @@
+// if
+
+// agar true likha toh andar ka code execute hoga false likha toh nhi 
+
+// (conditions)
+
+//  (iske andar question aega us question ka jo bhi ansewr hoga wo true ya false k format mein hoga)
+
+if (true) {
+
+}
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+//  = assignment ke liye hota hai
+
+// === datatype bhi check krta hai == nhi 
---
+++ basics_3/01_functions.js
@@ -0,0 +1,32 @@
+function sayMyName (){
+    console.log("D");
+    console.log("E");
+    console.log("V");
+    console.log("A");
+    console.log("N");
+    console.log("S");
+    console.log("H");
+    
+}
+
+sayMyName //reference
+sayMyName () //execution
+
+
+// idhar number1, number2 parameters hain 
+function addTwoNumbers (number1, number2 ){
+
+console.log(number1 + number2);
+
+}
+
+addTwoNumbers(3, null)
+// 3, null argument hain 
+
+// function ko variable k jaise bhi declare kr skte hain 
+
+// lekin agar value ki tarah declare kroge toh function ko declaration se pehle execute nhi kr skte 
+
+
+
+// ----------------------------------- functions revise krlio bohot sare points hain --------------------
