460b6c66732d339b278ea1062b0c3890221dd0fd
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2024-12-08 15:12:50 +0000

    arrays done ache se

--- basics_2/02_methods_of_arrays.js
+++ basics_2/02_methods_of_arrays.js
@@ -1,33 +1,51 @@
-const marvel_heroes = ["Iron Man", "Thor", "Captian America"]
-const dc_heroes = ["Super Man", "Bat Man", "Flash"]
+// const marvel_heroes = ["Iron Man", "Thor", "Captian America"]
+// const dc_heroes = ["Super Man", "Bat Man", "Flash"]
 
-// marvel_heroes.push(dc_heroes)
-console.log(marvel_heroes)
+const arr1 = ["x", "y", "z"]
+const arr2 = ["a", "b", "c"]
 
-// isse marvel_heroes array ke andar dc_heroes array ki form mein add ho jaega 
+// arr1.push(arr2)
+// console.log(arr1);
+// console.log(arr1[3][2]);
 
-// console.log(marvel_heroes[3][1]);
+// const arr3 = arr1.concat(arr2)
+// console.log(arr3);
 
-// concat use krte hue dhyan dena kyuki wo dono arrays ko combine krke new array deta hai 
+const newArr = [...arr1, ...arr2]
+console.log(newArr);
 
-const sexyArray = marvel_heroes.concat(dc_heroes)
-console.log(sexyArray);
+const desiArray = ['d','i','v','y', ['a','n','s',['h','u']]]
+const devanshArray = desiArray.flat(Infinity)
+console.log(devanshArray);
 
-// --------------------------------Same cheeze krne ka ek or tareeka -------------------------------------
 
-const nayaArray = [...marvel_heroes, ...dc_heroes]
-console.log(nayaArray);
+// // marvel_heroes.push(dc_heroes)
+// console.log(marvel_heroes)
 
-// -----------------------------isko spread bolte h ye sabhi array k elements ko alag alag kr deta h 
+// // isse marvel_heroes array ke andar dc_heroes array ki form mein add ho jaega 
 
-const chutiyaArray = [1,2,3,4,[5,6,7,[8,9,10]]]
-const bhadwaArray = chutiyaArray.flat(Infinity)
-console.log(bhadwaArray);
+// // console.log(marvel_heroes[3][1]);
 
-// agar kahi array k andr array k andar array etc ho toh ise use kro 
+// // concat use krte hue dhyan dena kyuki wo dono arrays ko combine krke new array deta hai 
+
+// const sexyArray = marvel_heroes.concat(dc_heroes)
+// console.log(sexyArray);
+
+// // --------------------------------Same cheeze krne ka ek or tareeka -------------------------------------
+
+// const nayaArray = [...marvel_heroes, ...dc_heroes]
+// console.log(nayaArray);
+
+// // -----------------------------isko spread bolte h ye sabhi array k elements ko alag alag kr deta h 
+
+// const chutiyaArray = [1,2,3,4,[5,6,7,[8,9,10]]]
+// const bhadwaArray = chutiyaArray.flat(Infinity)
+// console.log(bhadwaArray);
+
+// // agar kahi array k andr array k andar array etc ho toh ise use kro 
 
 console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
+console.log(Array.from("Devansh"))
 console.log(Array.from({name: "hitesh"})) // interesting
 
 let score1 = 100
