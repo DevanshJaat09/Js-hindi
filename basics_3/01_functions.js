46ac58a915509e1c535780be25cace1c35d4512b
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2024-12-14 08:49:36 +0000

    functions part 1

--- basics_3/01_functions.js
+++ basics_3/01_functions.js
@@ -30,3 +30,80 @@ addTwoNumbers(3, null)
 
 
 // ----------------------------------- functions revise krlio bohot sare points hain --------------------
+// return keyword likhne ke baad ka code execute nhi hoga
+
+function loginUserMessage (username){
+    return `${username} Just Logged In`
+}
+
+loginUserMessage
+console.log(loginUserMessage('Devansh'))
+// console.log(loginUserMessage(Devansh))
+// error devansh is not defined 
+
+console.log(loginUserMessage(''))
+//   Just Logged In
+
+console.log(loginUserMessage())
+// undefined Just Logged In 
+
+
+// "" and undefined are considered as false values 
+
+
+function userLoggedIn (userid){
+    if(userid === undefined){
+        console.log("Please Enter A UserID");
+        return
+    }
+    return `${userid} Just Logged In` 
+}
+
+console.log(userLoggedIn());
+// Please Enter A UserID
+// undefined
+
+function userLoggedInTwo(userid) {
+    if(!userid){
+        console.log("Please Enter A UserID");
+        return       
+    }
+    return `${userid} Just Logged In`
+}
+
+console.log(userLoggedInTwo());
+// Please Enter A UserID
+// undefined
+
+
+// default value dene ke liye 
+
+function userLoggedInThree (username = "Sammy"){
+    return `${username} Just Logged In`
+}
+
+console.log(userLoggedInThree());
+console.log(userLoggedInThree("Devansh"));
+
+
+function addNumbers (number1, number2) {
+    console.log(number1 + number2);
+    
+}
+
+const result = addNumbers(3,4)
+console.log("Result: ", result);
+// 7
+// Result:  undefined
+// console likhne se sirf value print hoti hai value function ko return nahi hoti 
+
+
+// right way 
+
+function addingNumbers (numb1, numb2) {
+    let output = numb1 + numb2
+    return output
+}
+
+let output = addingNumbers(3,5)
+console.log("Result: ", output);
