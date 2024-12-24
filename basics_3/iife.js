1540e551de1fa2c6354588381d71d92ef08fbdee
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2024-12-24 07:19:46 +0000

    iife explained

--- basics_3/iife.js
+++ basics_3/iife.js
@@ -1,5 +1,7 @@
 // IIFE Immediately Invoked Function Expression 
 
+// IIFE is used to protect the function from the pollution of global scope 
+
 (function devansh (){
     // named IIFE 
     console.log(`DB Connected`);
